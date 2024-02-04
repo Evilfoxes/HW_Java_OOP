31c24a4d6ce93f6040d2dd5f6f77107d6f9f1d82
evilfoxes <evilfoxes@mail.ru>
2024-02-04 16:13:27 +1100

    Save_All

--- src/Clients/Illness.java
+++ src/Clients/Illness.java
@@ -1,4 +1,11 @@
 package Clients;
 
 public class Illness {
+    String title;
+    public Illness(String title) {
+        this.title = title;
+    }
+    public String toString() {
+        return "Illness{" + "title='" + title + '\''+ '}';
+    }
 }
--- src/Clients/Owner.java
+++ src/Clients/Owner.java
@@ -1,4 +1,11 @@
 package Clients;
 
 public class Owner {
+    String fullName;
+    public Owner(String fullName) {
+        this.fullName = fullName;
+    }
+    public String toString() {
+        return "Owner{" + "fullName='" + fullName + '\''+ '}';
+    }
 }
